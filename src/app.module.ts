6f730eca59101d97244349253cf3f34c4649e55c
im <83586607+exodie@users.noreply.github.com>
2023-10-19 00:03:06 +0400

    update app.module.ts
    - Remove public test token
    - Regenerate token and paste to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ const localSession = new LocalSession({ database: 'sessing.json' });
   imports: [
     TelegrafModule.forRoot({
       middlewares: [localSession.middleware()],
-      token: '6386287837:AAHgE3T4z_8GaK5LFVscxrQNWkn32Hq4SXA',
+      token: process.env.TOKEN,
     }),
   ],
   providers: [AppService, AppUpdate],
